fe15ed1477725b56baf727c27dd95d6f8bd05339
Constance De Voogd <cde-voog@c1r6p2.42lehavre.fr>
2023-04-17 23:43:36 +0200

    fffgh

---
+++ ft_memcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 23:38:25 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/04/17 23:40:21 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+int     ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*prem;
+        const unsigned char	*scd;
+	size_t				i;
+
+	i = 0;
+	prem = (const unsigned char *)s1;
+	scd = (const unsigned char *)s2;
+	while (i < n)
+	{
+		if (prem[i] != scd[i])
+			return (prem[i] - scd[i]);
+		i++;
+	}
+	return (0);
+}

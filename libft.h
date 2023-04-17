09be127ca909b094ebc69e6add0b6b583ee4b794
Constance De Voogd <cde-voog@c1r6p2.42lehavre.fr>
2023-04-18 00:12:14 +0200

    fws

--- libft.h
+++ libft.h
@@ -6,16 +6,13 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 23:01:52 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/16 23:13:23 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/17 19:16:25 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
-# define FT_INIT_MIN -2147483648
-# define FT_INIT_MAX 2147483648
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -30,11 +27,16 @@ int	ft_tolower(int c);
 int	ft_toupper(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif

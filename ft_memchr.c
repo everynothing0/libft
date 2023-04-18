c7bcdda0dab5575cdf752816624e1489f3ed751d
Constance De Voogd <cde-voog@c1r6p2.42lehavre.fr>
2023-04-18 02:51:05 +0200

    ersbrgd

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,24 +6,25 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 18:55:33 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/17 23:37:39 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/18 02:32:23 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str;
 
 	if (!s)
 		return (NULL);
-	i = 0;
-	while (i < n)
+	i = -1;
+	str = (unsigned char *)s;
+	while (++i < n)
 	{
-		if (*(unsigned char *)(s + 1) == (unsigned char)c)
-			return ((void *)(s + i++));
+		if (str[i] == (unsigned char)c)
+			return (str + i);
 	}
 	return (NULL);
 }

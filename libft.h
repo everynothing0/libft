431167ec87b0b850d883be1c569fbfa0c3ded998
Constance De Voogd <cde-voog@c2r1p2.42lehavre.fr>
2023-04-19 13:37:15 +0200

    lin

--- libft.h
+++ libft.h
@@ -32,6 +32,8 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char c, int fd);
 void	*ft_memset(void *s, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);

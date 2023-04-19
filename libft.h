9b618d9413d6b24de3ff9f5e69d83c0c3e3cb26f
Constance De Voogd <cde-voog@c2r1p2.42lehavre.fr>
2023-04-19 13:20:11 +0200

    lin

--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@
 # include <unistd.h>
 # include <limits.h>
 
+int	ft_atoi(const char *str);
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
@@ -25,6 +26,7 @@ int	ft_isdigit(int c);
 int	ft_isprint(int c);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
+char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
@@ -34,7 +36,8 @@ void	*ft_memset(void *s, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);

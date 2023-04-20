054be43d82fca4beaaa76ef95663d53d3bf78337
Constance De Voogd <cde-voog@c2r10p1.42lehavre.fr>
2023-04-20 21:42:53 +0200

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 20:52:21 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/16 22:35:44 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/20 21:33:59 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-		if (*s++ == (char)c)
-			return ((char *)s - 1);
-	if (c == '\0')
-		return ((char *)s);
-	return (0);
+	char	car;
+	int				cnt;
+
+	car = c;
+	cnt = 0;
+	while (s[cnt] != car)
+	{
+		if (s[cnt] == '\0')
+			return (NULL);
+		cnt++;
+	}
+	return ((char *)s + cnt);
 }

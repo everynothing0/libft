82c0c6d390696d4331889318a78901a30e4b2638
Constance De Voogd <cde-voog@c1r7p7.42lehavre.fr>
2023-04-21 16:11:03 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,30 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/17 18:55:33 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/20 21:40:40 by cde-voog         ###   ########.fr       */
+/*   Created: 2023/04/21 15:29:27 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/04/21 16:08:23 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t			i;
-	unsigned char	*str;
+	size_t	i;
+	size_t	j;
+	char	*res;
 
 	i = 0;
-	str = (unsigned char *)s;
-	while (i < n)
+	j = 0;
+	res = malloc(sizeof(char) + (ft_strlen(s1) + (ft_strlen(s2) + 1)));
+	if (!res)
+		return (NULL);
+	while (s1[i] != '\0' && s1[i] != '\0')
 	{
-		if (*str == (unsigned char)c)
-			return ((void *)str);
-		str++;
+		res[i] = s1[i];
 		i++;
 	}
-	return (NULL);
+	while (s2[j] != '\0' && s2[j] != '\0')
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
 }

a1e351ab78277aa30458f9abc61d63269f239b63
Constance De Voogd <cde-voog@c1r1p10.42lehavre.fr>
2023-04-23 19:33:42 +0200

    trim-trimmmm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 03:37:29 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/23 03:59:48 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/23 19:23:35 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,16 @@ static int	ft_isinset(char c, const char *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	start;
+	size_t	end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_isinset(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_isinset(s1[end - 1], set))
+		end--;
+	return (ft_substr(s1, start, end - start));
 }

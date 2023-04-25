6395b48b93e1d9b4adc1c98388f1d462db7e2c7f
Constance De Voogd <cde-voog@c1r4p13.42lehavre.fr>
2023-04-25 03:48:10 +0200

    fwg

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,31 +6,22 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 22:56:10 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/16 23:00:02 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/20 20:30:58 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	c_char(char c1, char c2)
-{
-	if ((unsigned char)c1 != (unsigned char)c2)
-		return ((unsigned char)c1 - (unsigned char)c2);
-	return (0);
-}
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
-		if (c_char(s1[i], s2[i]))
-			return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if (i < n)
-		return (c_char(s1[i], s2[i]));
 	return (0);
 }

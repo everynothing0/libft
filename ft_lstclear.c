83b153e62764287a578eded8aa69412d57a687ab
Constance De Voogd <cde-voog@c1r4p13.42lehavre.fr>
2023-04-25 05:51:57 +0200

    clear

---
+++ ft_lstclear.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 05:38:45 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/04/25 05:51:35 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next;
+
+	if (lst && del)
+	{
+		current = *lst;
+		while (current)
+		{
+			next = current->next;
+			ft_lstdelone(current, del);
+			current = next;
+		}
+		*lst = NULL;
+	}
+}

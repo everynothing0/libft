8e54041263c75336969f310e37946231b26c1b34
Constance De Voogd <cde-voog@c1r4p13.42lehavre.fr>
2023-04-25 05:22:59 +0200

    lstadd back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 05:05:52 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/04/25 05:15:07 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*end;
+
+	if (lst == NULL)
+		return (NULL);
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return (NULL);
+	}
+	else
+	{
+		end = ft_lstlast(*lst);
+		end->next = new;
+	}
+}

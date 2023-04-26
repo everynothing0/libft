e4c714c6f439eadeef2d9df4ddf908a759b7c1fd
Constance De Voogd <cde-voog@c1r3p12.42lehavre.fr>
2023-04-26 03:21:18 +0200

    lstmap nul

---
+++ ft_lstmap.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 02:54:50 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/04/26 03:20:00 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+
+	if (!lst || !f || !del)
+		return (0);
+	new_list = ft_lstnew(f(lst->content));
+	if (!new_list)
+		return (0);
+	lst = lst->next;
+	while (lst)
+	{
+		new_list->next = ft_lstnew(f(lst->content));
+		if (!new_list->next)
+		{
+			ft_lstclear(&new_list, del);
+			return (0) ;
+		}
+		new_list = new_list->next;
+		lst = lst->next;
+	}
+	new_list->next = NULL;
+	return (new_list);
+}

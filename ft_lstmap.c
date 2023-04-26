ad83995d0462e314c00e53e0698b136e7d9e65b6
Constance De Voogd <cde-voog@c2r3p1.42lehavre.fr>
2023-04-26 20:24:02 +0200

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 02:54:50 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/26 03:20:00 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/04/26 20:21:50 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
+	t_list	*save;
 
 	if (!lst || !f || !del)
 		return (0);
@@ -22,17 +23,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!new_list)
 		return (0);
 	lst = lst->next;
+	save = new_list;
 	while (lst)
 	{
 		new_list->next = ft_lstnew(f(lst->content));
 		if (!new_list->next)
 		{
-			ft_lstclear(&new_list, del);
-			return (0) ;
+			ft_lstclear(&save, del);
+			return (0);
 		}
 		new_list = new_list->next;
 		lst = lst->next;
 	}
-	new_list->next = NULL;
-	return (new_list);
+	new_list = new_list->next;
+	return (save);
 }

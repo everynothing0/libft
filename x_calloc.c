abbbc6b0e4e7963eae1f15dce44535d24425aca9
cde-voog <nozepastabos@gmail.com>
2023-08-25 18:23:18 +0200

    Xcalloc err

--- x_calloc.c
+++ x_calloc.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   x_calloc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 12:34:50 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/08/24 12:35:20 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 // for mallocs and bzerros dispkays errors
-// message and exits in case of failure
+// message and exits in case of failure, she's not standard lib
 
 void	*x_calloc(size_t size)
 {

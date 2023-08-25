c8fe549e745e45af2ea49deda04a3fa3450f91ef
cde-voog <nozepastabos@gmail.com>
2023-08-25 18:22:43 +0200

    arrlen

--- ft_arrlen.c
+++ ft_arrlen.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_arrlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 12:34:06 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/08/24 12:34:37 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-// returns the number of elements in a string array
+// returns the number of elements in a string array, she's not to standard libft
 
 int	ft_arrlen(char **str)
 {
